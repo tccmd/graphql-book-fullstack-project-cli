1ed21ab063d594df0ac4fa8f88d2afd36626a97f
tccmd <tccmd52@gmail.com>
2024-11-21 17:42:09 +0900

    Apollo client, graphql, AWS_ EB, S3 successfully deploy

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -1,38 +1,29 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+// Apollo Client와 관련된 모듈을 import
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ChakraProvider, Box, Text, theme } from "@chakra-ui/react";
+import * as React from "react";
+import FilmList from "./components/film/FilmList";
 
+// Apollo Client 인스턴스 생성
+const apolloClient = new ApolloClient({
+  // GraphQL 서버의 URI를 설정 (로컬에서 실행 중인 서버를 가리킴)
+  uri: `${process.env.REACT_APP_API_HOST}/graphql`,
+  // Apollo Client의 캐시 정책 설정 (InMemoryCache 사용)
+  cache: new InMemoryCache(),
+});
+
+// React 컴포넌트 정의
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+  // ApolloProvider로 앱 전체에 Apollo Client를 제공
+  <ApolloProvider client={apolloClient}>
+    {/* ChakraProvider로 앱 전체에 Chakra UI 테마를 적용 */}
+    <ChakraProvider theme={theme}>
+      {/* Chakra UI의 Box 컴포넌트를 사용해 텍스트를 중앙 정렬 */}
+      <Box textAlign="center" fontSize="xl">
+        {/* Chakra UI의 Text 컴포넌트를 사용해 텍스트 표시 */}
+        <Text>Ghibli GraphQL</Text>
+        <FilmList />
+      </Box>
+    </ChakraProvider>
+  </ApolloProvider>
+);
---
+++ project/web/src/generated/graphql.tsx
@@ -0,0 +1,98 @@
+/* eslint-disable */
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Film = {
+  __typename?: 'Film';
+  /** 영화 줄거리 및 설명 */
+  description: Scalars['String']['output'];
+  /** 제작자 고유 아이디 */
+  director_id: Scalars['Int']['output'];
+  /** 영화 장르 */
+  genre: Scalars['String']['output'];
+  /** 영화 고유 아이디 */
+  id: Scalars['Int']['output'];
+  /** 포스터 이미지 URL */
+  posterImg: Scalars['String']['output'];
+  /** 개봉일 */
+  release: Scalars['String']['output'];
+  /** 영화 러닝 타임, minute */
+  runningTime: Scalars['Float']['output'];
+  /** 영화 부제목 */
+  subtitle?: Maybe<Scalars['String']['output']>;
+  /** 영화 제목 */
+  title: Scalars['String']['output'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  films: Array<Film>;
+};
+
+export type FilmsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type FilmsQuery = { __typename?: 'Query', films: Array<{ __typename?: 'Film', id: number, title: string, subtitle?: string | null, runningTime: number, director_id: number, release: string, posterImg: string }> };
+
+
+export const FilmsDocument = gql`
+    query Films {
+  films {
+    id
+    title
+    subtitle
+    runningTime
+    director_id
+    release
+    posterImg
+  }
+}
+    `;
+
+/**
+ * __useFilmsQuery__
+ *
+ * To run a query within a React component, call `useFilmsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useFilmsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useFilmsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useFilmsQuery(baseOptions?: Apollo.QueryHookOptions<FilmsQuery, FilmsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<FilmsQuery, FilmsQueryVariables>(FilmsDocument, options);
+      }
+export function useFilmsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<FilmsQuery, FilmsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<FilmsQuery, FilmsQueryVariables>(FilmsDocument, options);
+        }
+export function useFilmsSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<FilmsQuery, FilmsQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<FilmsQuery, FilmsQueryVariables>(FilmsDocument, options);
+        }
+export type FilmsQueryHookResult = ReturnType<typeof useFilmsQuery>;
+export type FilmsLazyQueryHookResult = ReturnType<typeof useFilmsLazyQuery>;
+export type FilmsSuspenseQueryHookResult = ReturnType<typeof useFilmsSuspenseQuery>;
+export type FilmsQueryResult = Apollo.QueryResult<FilmsQuery, FilmsQueryVariables>;
